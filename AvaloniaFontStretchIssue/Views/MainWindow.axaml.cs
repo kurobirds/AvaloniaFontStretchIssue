e16c913ac4013be14e7bdb7281d71333198bf3dc
Sang Thái Minh <sang024854@gmail.com>
2024-06-05 18:20:05 +0700

    Reproduce issue

---
+++ AvaloniaFontStretchIssue/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaFontStretchIssue.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
